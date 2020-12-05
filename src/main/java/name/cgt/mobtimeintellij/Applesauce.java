4811270a939a080fda009c2368ab358ae32dc1ce
Christoffer G. Thomsen <chris@cgt.name>
2020-12-05 16:51:58 +0100

    Move inner class to top level

---
+++ src/main/java/name/cgt/mobtimeintellij/Applesauce.java
@@ -0,0 +1,24 @@
+package name.cgt.mobtimeintellij;
+
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.ui.Messages;
+
+import java.time.Duration;
+import java.util.Timer;
+import java.util.TimerTask;
+
+class Applesauce {
+    static void invoke() {
+        final var timer = new Timer(true);
+        final var showTimerExpired = new TimerTask() {
+            @Override
+            public void run() {
+                final var app = ApplicationManager.getApplication();
+                app.invokeLater(() ->
+                  Messages.showInfoMessage("The timer has expired.", "Rotate!")
+                );
+            }
+        };
+        timer.schedule(showTimerExpired, Duration.ofSeconds(5).toMillis());
+    }
+}
--- src/main/java/name/cgt/mobtimeintellij/StartTimer.java
+++ src/main/java/name/cgt/mobtimeintellij/StartTimer.java
@@ -2,13 +2,7 @@ package name.cgt.mobtimeintellij;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
-
-import java.time.Duration;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class StartTimer extends AnAction {
     @Override
@@ -21,19 +15,4 @@ public class StartTimer extends AnAction {
         return false;
     }
 
-    private static class Applesauce {
-        static void invoke() {
-            final var timer = new Timer(true);
-            final var showTimerExpired = new TimerTask() {
-                @Override
-                public void run() {
-                    final var app = ApplicationManager.getApplication();
-                    app.invokeLater(() ->
-                      Messages.showInfoMessage("The timer has expired.", "Rotate!")
-                    );
-                }
-            };
-            timer.schedule(showTimerExpired, Duration.ofSeconds(5).toMillis());
-        }
-    }
 }
