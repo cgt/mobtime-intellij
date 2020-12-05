68c6050518bafcf527b3bbe367aeb5f6553f204b
Christoffer G. Thomsen <chris@cgt.name>
2020-12-05 15:22:21 +0100

    Example dialog

--- src/main/java/name/cgt/mobtimeintellij/MyAction.java
+++ src/main/java/name/cgt/mobtimeintellij/MyAction.java
@@ -1,8 +1,5 @@
 package name.cgt.mobtimeintellij;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
@@ -10,14 +7,8 @@ import org.jetbrains.annotations.NotNull;
 public class MyAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        Notifications.Bus.notify(
-          new Notification(
-            "myng",
-            "My title",
-            "My content",
-            NotificationType.INFORMATION
-          )
-        );
+        final var userClickedOK = new MyDialog().showAndGet();
+        System.out.println("userClickedOK = " + userClickedOK);
     }
 
     @Override
