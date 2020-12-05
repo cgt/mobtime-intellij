2d1e27de0be4d1e38f3d9869fc724b610016bf76
Christoffer G. Thomsen <chris@cgt.name>
2020-12-05 16:21:05 +0100

    Start Timer action starts a timer that expires after 5 seconds

--- src/main/java/name/cgt/mobtimeintellij/StartTimer.java
+++ src/main/java/name/cgt/mobtimeintellij/StartTimer.java
@@ -2,13 +2,28 @@ package name.cgt.mobtimeintellij;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class StartTimer extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        Messages.showInfoMessage("The timer has expired.", "Rotate!");
+        final var timer = new Timer(true);
+        final var task = new TimerTask() {
+            @Override
+            public void run() {
+                final var app = ApplicationManager.getApplication();
+                app.invokeLater(() ->
+                  Messages.showInfoMessage("The timer has expired.", "Rotate!")
+                );
+            }
+        };
+        timer.schedule(task, Duration.ofSeconds(5).toMillis());
     }
 
     @Override
