5fdce7907dcb9777a1040eedce9fa4ae37207ebb
Christoffer G. Thomsen <chris@cgt.name>
2020-12-05 16:54:48 +0100

    Introduce field

--- src/main/java/name/cgt/mobtimeintellij/TimerService.java
+++ src/main/java/name/cgt/mobtimeintellij/TimerService.java
@@ -8,8 +8,12 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 class TimerService {
+
+    private Timer timer;
+
     void startTimer() {
-        final var timer = new Timer(true);
+        this.timer = new Timer(true);
+        final var timer = this.timer;
         final var showTimerExpired = new TimerTask() {
             @Override
             public void run() {
