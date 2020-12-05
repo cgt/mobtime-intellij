81ec1c2680a86e396c725dac9dc67e87e61f6177
Christoffer G. Thomsen <chris@cgt.name>
2020-12-05 14:44:02 +0100

    Show info message dialog

--- src/main/java/name/cgt/mobtimeintellij/MyAction.java
+++ src/main/java/name/cgt/mobtimeintellij/MyAction.java
@@ -2,12 +2,14 @@ package name.cgt.mobtimeintellij;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
 public class MyAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         System.out.println("Execute MyAction again!!");
+        Messages.showInfoMessage("Message from MyAction", "An Excellent Dialog");
     }
 
     @Override
