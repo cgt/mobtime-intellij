ce52304b1d812fda893646c169b15143ef494717
Christoffer G. Thomsen <chris@cgt.name>
2020-12-05 17:03:04 +0100

    Show "Timer ready" label in status bar

---
+++ src/main/java/name/cgt/mobtimeintellij/StatusBarTimer.java
@@ -0,0 +1,66 @@
+package name.cgt.mobtimeintellij;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.NlsContexts;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.StatusBarWidget;
+import com.intellij.openapi.wm.impl.status.EditorBasedWidget;
+import com.intellij.util.Consumer;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.awt.event.MouseEvent;
+
+public class StatusBarTimer extends EditorBasedWidget implements StatusBarWidget.TextPresentation {
+    @NotNull
+    public static final String id = "StatusBarTimer";
+
+    protected StatusBarTimer(@NotNull Project project) {
+        super(project);
+    }
+
+    @Override
+    @NonNls
+    @NotNull
+    public String ID() {
+        return id;
+    }
+
+    @Override
+    public void install(@NotNull StatusBar statusBar) {
+        super.install(statusBar);
+        statusBar.updateWidget(ID());
+    }
+
+    @Override
+    @Nullable
+    public WidgetPresentation getPresentation() {
+        return this;
+    }
+
+    @Override
+    @NotNull
+    @NlsContexts.Label
+    public String getText() {
+        return "Timer ready";
+    }
+
+    @Override
+    public float getAlignment() {
+        return 0;
+    }
+
+    @Override
+    @Nullable
+    @NlsContexts.Tooltip
+    public String getTooltipText() {
+        return "Mobtime status";
+    }
+
+    @Override
+    @Nullable
+    public Consumer<MouseEvent> getClickConsumer() {
+        return null;
+    }
+}
---
+++ src/main/java/name/cgt/mobtimeintellij/StatusBarTimerFactory.java
@@ -0,0 +1,47 @@
+package name.cgt.mobtimeintellij;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.StatusBarWidget;
+import com.intellij.openapi.wm.StatusBarWidgetFactory;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class StatusBarTimerFactory implements StatusBarWidgetFactory {
+    @Override
+    @NonNls
+    @NotNull
+    public String getId() {
+        return StatusBarTimer.id;
+    }
+
+    @Override
+    @Nls
+    @NotNull
+    public String getDisplayName() {
+        return "Mobtime countdown";
+    }
+
+    @Override
+    public boolean isAvailable(@NotNull Project project) {
+        return true;
+    }
+
+    @Override
+    @NotNull
+    public StatusBarWidget createWidget(@NotNull Project project) {
+        return new StatusBarTimer(project);
+    }
+
+    @Override
+    public void disposeWidget(@NotNull StatusBarWidget statusBarWidget) {
+        Disposer.dispose(statusBarWidget);
+    }
+
+    @Override
+    public boolean canBeEnabledOn(@NotNull StatusBar statusBar) {
+        return true;
+    }
+}
