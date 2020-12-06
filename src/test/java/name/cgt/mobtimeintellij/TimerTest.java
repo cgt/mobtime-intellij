63c8e96a1029b355c69b3998d370bba1ebcb3f59
Christoffer G. Thomsen <chris@cgt.name>
2020-12-06 14:57:57 +0100

    Timer updates display on tick event
    Which would be sent by some outside source.

--- src/test/java/name/cgt/mobtimeintellij/TimerTest.java
+++ src/test/java/name/cgt/mobtimeintellij/TimerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.time.Duration;
+import java.time.Instant;
 
 public class TimerTest {
     @RegisterExtension
@@ -29,11 +30,32 @@ public class TimerTest {
             oneOf(display).timeRemaining(Duration.ofSeconds(60));
         }});
 
-        timer.start(Duration.ofSeconds(60));
+        timer.start(Instant.now(), Duration.ofSeconds(60));
+    }
+
+    @Test
+    public void on_tick_update_and_display_time_remaining() {
+        final var t1 = Instant.now();
+        final var t2 = t1.plusSeconds(1);
+
+        context.checking(new Expectations() {{
+            final var update = context.sequence("update");
+
+            oneOf(display).timeRemaining(Duration.ofSeconds(60));
+            inSequence(update);
+
+            oneOf(display).timeRemaining(Duration.ofSeconds(59));
+            inSequence(update);
+        }});
+
+        timer.start(t1, Duration.ofSeconds(60));
+        timer.tick(t2);
     }
 
     private static class Timer {
         private final Display display;
+        private Instant startTime;
+        private Duration duration;
 
         public Timer(Display display) {
             this.display = display;
@@ -43,8 +65,16 @@ public class TimerTest {
             display.timeRemaining(Duration.ZERO);
         }
 
-        public void start(Duration time) {
+        public void start(Instant now, Duration time) {
+            startTime = now;
+            duration = time;
             display.timeRemaining(time);
+        }
+
+        public void tick(Instant now) {
+            final var elapsed = Duration.between(startTime, now);
+            final var remaining = duration.minus(elapsed);
+            display.timeRemaining(remaining);
         }
     }
 
